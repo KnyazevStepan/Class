f6e3a5f6ebb3472403882fe81078223c02b662fb
Neto test <Knyazev18.step@mail.ru>
2025-08-20 14:59:35 +0700

    Task number two

--- py/homework.py
+++ py/homework.py
@@ -9,6 +9,31 @@ class Student:
 
     def add_courses(self, course_name):
         self.finished_courses.append(course_name)
+    
+    def rate_lecture(self, lecturer, course, grade):
+        # Проверяем, что lecturer является экземпляром класса Lecturer
+        if not isinstance(lecturer, Lecturer):
+            return 'Ошибка: Может оценивать только лекторов'
+        
+        # Проверяем, что студент записан на этот курс
+        if course not in self.courses_in_progress:
+            return 'Ошибка: Студент не записан на этот курс'
+        
+        # Проверяем, что лектор закреплен за этим курсом
+        if course not in lecturer.courses_attached:
+            return 'Ошибка: Лектор не закреплен за этим курсом'
+        
+        # Проверяем, что оценка в допустимом диапазоне (1-10)
+        if not (1 <= grade <= 10):
+            return 'Ошибка: Оценка должна быть от 1 до 10'
+        
+        # Добавляем оценку лектору
+        if course in lecturer.grades:
+            lecturer.grades[course].append(grade)
+        else:
+            lecturer.grades[course] = [grade]
+        
+        return None  # Успешное выполнение
 
                
 class Mentor:
@@ -17,42 +42,55 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
-    def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
-            if course in student.grades:
-                student.grades[course] += [grade]
-            else:
-                student.grades[course] = [grade]
-        else:
-            return 'Ошибка'
-
 
 class Lecturer(Mentor):
-    pass
+    """Класс для лекторов, наследуется от Mentor"""
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}  # Словарь для хранения оценок за лекции
 
 
 class Reviewer(Mentor):
-    pass
+    """Класс для экспертов, проверяющих домашние задания, наследуется от Mentor"""
+    
+    def rate_hw(self, student, course, grade):
+        # Проверяем, что student является экземпляром класса Student
+        if not isinstance(student, Student):
+            return 'Ошибка: Может оценивать только студентов'
+        
+        # Проверяем, что эксперт закреплен за этим курсом
+        if course not in self.courses_attached:
+            return 'Ошибка: Эксперт не закреплен за этим курсом'
+        
+        # Проверяем, что студент изучает этот курс
+        if course not in student.courses_in_progress:
+            return 'Ошибка: Студент не изучает этот курс'
+        
+        # Проверяем, что оценка в допустимом диапазоне (1-10)
+        if not (1 <= grade <= 10):
+            return 'Ошибка: Оценка должна быть от 1 до 10'
+        
+        # Добавляем оценку студенту
+        if course in student.grades:
+            student.grades[course].append(grade)
+        else:
+            student.grades[course] = [grade]
+        
+        return None  # Успешное выполнение
 
 
-# Создаем экземпляры классов
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
-
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
-
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-
-print(best_student.grades)
-
-# Тестируем наследование
+# Тестирование
 lecturer = Lecturer('Иван', 'Иванов')
 reviewer = Reviewer('Пётр', 'Петров')
+student = Student('Алёхина', 'Ольга', 'Ж')
 
-print(isinstance(lecturer, Mentor))  # True
-print(isinstance(reviewer, Mentor))  # True
-print(lecturer.courses_attached)     # []
-print(reviewer.courses_attached)     # []
+student.courses_in_progress += ['Python', 'Java']
+lecturer.courses_attached += ['Python', 'C++']
+reviewer.courses_attached += ['Python', 'C++']
+
+print(student.rate_lecture(lecturer, 'Python', 7))   # None - успешно
+print(student.rate_lecture(lecturer, 'Java', 8))     # Ошибка: Лектор не закреплен за этим курсом
+print(student.rate_lecture(lecturer, 'C++', 8))      # Ошибка: Студент не записан на этот курс
+print(student.rate_lecture(reviewer, 'Python', 6))   # Ошибка: Может оценивать только лекторов
+
+print(lecturer.grades)  # {'Python': [7]}
